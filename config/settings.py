a34d6c619b9ed14f50849b32e41da42483f5a5b2
Somi-Project <somisongbird03@gmail.com>
2025-03-16 09:11:04 -0400

    Initial commit of somi project

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# config/settings.py
+DEFAULT_MODEL = "llama3"
+DEFAULT_TEMP = 0.7
+
+# Twitter/X credentials for goat-x
+TWITTER_USERNAME = ""
+TWITTER_PASSWORD = ""
+
+# Telegram credential
+TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
