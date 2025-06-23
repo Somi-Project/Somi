7684798e9b0b7eb12a3f8caeb8a27e03b6a45472
Somi-Project <somisongbird03@gmail.com>
2025-06-22 20:08:58 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,17 +5,18 @@ INSTRUCT_MODEL = "phi4-mini-reasoning:3.8b"
 DEFAULT_TEMP = 0.9
 DISABLE_MEMORY_FOR_FINANCIAL = True
 
-TWITTER_USERNAME = "Somi_Ai_Friend"
-TWITTER_PASSWORD = "Bones.12"
+# Twitter login
+TWITTER_USERNAME = "placeholder"
+TWITTER_PASSWORD = "placeholder"
 # Twitter API credentials
 TWITTER_API = {
-    "api_key": "nwIkrAc784c8k3dbKVVdQ43NY",
-    "api_secret": "khJfgfse2jwaFQprQuqFR59O22WTbfgqnJJ2n4nW1Dyd2bs3zL",
-    "access_token": "1927304408624140289-c0CEf7v5HVMYvqcumMGEY2UVfufugX",
-    "access_token_secret": "fNGXefH78Gq1PS1vLZddx1ZQUvBzfaNXSXQD5955tHaeB",
-    "bearer_token": "AAAAAAAAAAAAAAAAAAAAAHu62gEAAAAA5nt8DHuKpdseOiL9oMvz9HBMylw%3DcfO9vFNlerjYSO3UD7fE7fBFqHuPXtvoJhNwfIwtaj05kx8YaH",
-    "client_id": "eHAzRHV0U3NxWXJ6c3FDTTh2WG06MTpjaQ",
-    "client_secret": "T-wT1Nvrmi2N97BdFAdpwVmGzLOET1IIOs7RyxneHOmhxEW0-4"
+    "api_key": "placeholder",
+    "api_secret": "placeholder",
+    "access_token": "placeholder",
+    "access_token_secret": "placeholder",
+    "bearer_token": "placeholder",
+    "client_id": "placeholder",
+    "client_secret": "placeholder"
 }
 # Twitter Auto-post and auto-reply intervals in minutes
 AUTO_POST_INTERVAL_MINUTES = 242
@@ -25,14 +26,14 @@ AUTO_REPLY_INTERVAL_MINUTES = 240
 AUTO_REPLY_INTERVAL_LOWER_VARIATION = 20  # -20 minutes
 AUTO_REPLY_INTERVAL_UPPER_VARIATION = 30  # +30 minutes
 
-TELEGRAM_BOT_TOKEN = "7745481120:AAH9xQOXP6OQJ5I_dHz_I653XXMhqx8lqXw"
-TELEGRAM_BOT_USERNAME = "@SomiAnalyticsBot"
-TELEGRAM_AGENT_ALIASES = ["Somi", "Somi bot", "Somi Agent", "Somi A.i.", "retard", "degenia"]
+TELEGRAM_BOT_TOKEN = "placeholder
+TELEGRAM_BOT_USERNAME = "placeholdert"
+TELEGRAM_AGENT_ALIASES = ["placeholder", "placeholder", "placeholder", "placeholder"]
 
 VISION_MODEL = "qwen2.5vl:3b"
 
 RAG_WEBSITES = [
-    "https://pmc.ncbi.nlm.nih.gov/articles/PMC7018407/"
+    "placeholder"
 ]
 
 # System timezone
